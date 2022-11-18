00a2fa931ee4cec59a47fbbab5701075d1945bc7
Polushin Dima <dima2004polushin2004@gmail.com>
2022-11-18 21:57:37 +0600

    Создал компонент app для добавление всех проектов для рендера

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {} from 'react';
+import FilmReel from "./containerfilmReel/App/App";
+import FilmReelF from "./containerFunctionfilmReel/App/App";
+import RandomJoke  from "./containerJoke/App/App"
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container-fluid'>
+      <FilmReel/>
+      <FilmReelF/>
+      <RandomJoke/>
     </div>
   );
-}
+};
 
 export default App;
